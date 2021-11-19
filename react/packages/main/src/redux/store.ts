1e506bcb85f66d5ea57a34006a3f9e3f82355465
riteshInvoidea <94535025+riteshInvoidea@users.noreply.github.com>
2021-11-19 11:08:39 +0530

    Add files via upload
    Test

--- react/packages/main/src/redux/store.ts
+++ react/packages/main/src/redux/store.ts
@@ -14,6 +14,7 @@ import storage from "redux-persist/lib/storage";
 import eventReducer from "./slices/event";
 import uiReducer from "./slices/ui";
 import chatUISlice from "./slices/chat-ui";
+import loginSlice from "./slices/login";
 
 const persistConfig = {
     key: "doar",
@@ -25,6 +26,7 @@ export const rootReducer = combineReducers({
     events: eventReducer,
     ui: uiReducer,
     chatUI: chatUISlice,
+    login: loginSlice,
 });
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
